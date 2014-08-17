41a2b7c83ce0eba2f4777df11243f324a54f0759
Thomas Watson Steen <w@tson.dk>
2014-08-17 03:59:54 +0200

    First take

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-mongo-restful
-=============
+# mongo-restful
 
 A very simple MongoDB RESTful proxy
+
+This is work in progress!
+
+## Installation
+
+```
+npm install -g watson/mongo-restful
+```
+
+## Usage
+
+Just run the following command:
+
+```
+MONGO_URI=... mongo-restful
+```
+
+Default port is 8080 (override with `PORT` environment variable)
+
+## API
+
+Query a collection:
+
+```
+GET /{collection}?q={query}&filter={filter}&sort={sort}&limit={limit}&skip={skip}
+```
+
+Request a specific document:
+
+```
+GET /{collection}/{_id}
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+'use strict';
+
+var util = require('util');
+var JSONStream = require('JSONStream');
+var db = require('mongojs')(process.env.MONGO_URI);
+var app = require('root')();
+
+var objectify = function (json) {
+  if ('$oid' in json)
+    return db.ObjectId(json['$oid']);
+  Object.keys(json).forEach(function (key) {
+    var value = json[key];
+    if (typeof value === 'object')
+      json[key] = objectify(value);
+  });
+  return json;
+};
+
+var parseJSON = function (json) {
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    return {};
+  }
+};
+
+var getCursor = function (collection, options) {
+  var query  = parseJSON(options.q),
+      filter = parseJSON(options.filter),
+      sort   = parseJSON(options.sort),
+      limit  = parseInt(options.limit || 0, 10),
+      skip   = parseInt(options.skip  || 0, 10),
+      collection = db.collection(collection),
+      cursor;
+
+  query = objectify(query);
+  console.log('querying: ' + util.inspect(query, false, null));
+  cursor = collection.find(query, filter).sort(sort).skip(skip);
+  if (limit) cursor.limit(limit);
+  return cursor;
+};
+
+var query = function (req, res) {
+  var cursor = getCursor(req.params.collection, req.query);
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  cursor.pipe(JSONStream.stringify()).pipe(res);
+};
+
+var get = function (req, res) {
+  var id = db.ObjectId(req.params.id);
+  db.collection(req.params.collection).findOne({ _id: id }, function (err, doc) {
+    // TODO: Handle error
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.end(JSON.stringify(doc));
+  });
+};
+
+app.get('/{collection}', query);
+app.get('/{collection}/{id}', get);
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Server running on port', app.address().port);
+});
