a8cdb82f3c2b37e81d8e5cfbd1c9f5e783d60257
Thomas Watson Steen <w@tson.dk>
2014-08-17 17:20:19 +0200

    Add tests

---
+++ test.js
@@ -0,0 +1,101 @@
+'use strict';
+
+process.env.PORT = 8081;
+process.env.MONGO_URI = 'localhost/mongo-restful-test';
+
+// start the server
+require('./');
+
+var http = require('http');
+var querystring = require('querystring');
+var test = require('tape');
+var db = require('mongojs')(process.env.MONGO_URI, ['test']);
+
+var data = [
+  { _id: 1, foo: 1 },
+  { _id: 2, foo: 2 },
+  { _id: 3, foo: 2 },
+  { _id: 4, foo: new Date() },
+  { _id: 5, foo: 'text' },
+  { bar: 1 }
+];
+
+var bootstrap = function (callback) {
+  db.test.remove({}, function (err) {
+    if (err) throw err;
+    db.test.insert(data, function (err, res) {
+      if (err) throw err;
+      callback();
+    });
+  });
+};
+
+var find = function (collection, options, expected, t) {
+  if (expected instanceof test.Test) return find(collection, null, options, expected);
+  if (options) {
+    if (options.q)      options.q      = JSON.stringify(options.q);
+    if (options.filter) options.filter = JSON.stringify(options.filter);
+    if (options.sort)   options.sort   = JSON.stringify(options.sort);
+  }
+  var url = 'http://localhost:' + process.env.PORT + '/' + collection +
+            (options ? '?' + querystring.stringify(options) : '');
+  http.get(url, function (res) {
+    var buffers = [];
+    res.on('data', buffers.push.bind(buffers));
+    res.on('end', function () {
+      var result = JSON.parse(Buffer.concat(buffers));
+      t.deepEqual(result, expected);
+      t.end();
+    });
+  });
+};
+
+bootstrap(function () {
+  var date = data[3].foo = data[3].foo.toISOString();
+  var oid = data[data.length-1]._id = data[data.length-1]._id.toString();
+
+  test('should return everything if no query are given', function (t) {
+    find('test', data, t);
+  });
+
+  test('should return an empty array if the collection does not exist', function (t) {
+    find('nowhere', [], t);
+  });
+
+  test('should query', function (t) {
+    find('test', { q: { foo: 2 } }, [{ _id: 2, foo: 2 }, { _id: 3, foo: 2 }], t);
+  });
+
+  test('should limit', function (t) {
+    find('test', { q: { foo: 2 }, limit: 1 }, [{ _id: 2, foo: 2 }], t);
+  });
+
+  test('should skip', function (t) {
+    find('test', { q: { foo: 2 }, limit: 1, skip: 1 }, [{ _id: 3, foo: 2 }], t);
+  });
+
+  test('should sort', function (t) {
+    find('test', { q: { foo: 2 }, sort: { _id: -1 } }, [{ _id: 3, foo: 2 }, { _id: 2, foo: 2 }], t);
+  });
+
+  test('should filter', function (t) {
+    find('test', { q: { foo: 2 }, filter: { _id: 1 } }, [{ _id: 2 }, { _id: 3 }], t);
+  });
+
+  test('should allow querying for an ObjectId', function (t) {
+    find('test', { q: { $oid: oid } }, [{ _id: oid, bar: 1 }], t);
+  });
+
+  test('should allow querying for a date', function (t) {
+    find('test', { q: { foo: { $date: date } } }, [{ _id: 4, foo: date }], t);
+  });
+
+  test('should allow querying with a regular expression', function (t) {
+    find('test', { q: { foo: { $regex: 'XT$', $options: 'i' } } }, [{ _id: 5, foo: 'text' }], t);
+  });
+
+  test('end', function (t) {
+    t.end();
+    process.exit();
+  });
+});
